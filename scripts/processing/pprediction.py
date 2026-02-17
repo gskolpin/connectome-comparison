43ceb90fe9095b25fea11f5137b4421645eb29d0
Griffyn Skye Kolpin <gkolpin@n3263.hyak.local>
2026-02-17 11:30:12 -0800

    add cpm, ridge, and fracridge

--- scripts/processing/pprediction.py
+++ scripts/processing/pprediction.py
@@ -34,8 +34,15 @@ from sklearn.model_selection import KFold, GroupKFold
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import LassoCV
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import RidgeCV
+from sklearn.feature_selection import r_regression
 from sklearn.metrics import r2_score
+from sklearn.model_selection import cross_val_predict
 from sklearn.pipeline import Pipeline
+from scipy.stats import pearsonr
+
+from fracridge import fracridge, FracRidgeRegressor
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -50,7 +57,7 @@ con_model = 'lassoBIC_blocks'
 #filepath for the predictor or just all the phenotype data (need to make a space for those in scrubbed)
 p_filepath = '/gscratch/scrubbed/gkolpin/phenotype_data/study-PNC_desc-participants.tsv'
 scaler = StandardScaler() #making it normal so things work with it
-sublist_filepath = '/gscratch/escience/gkolpin/connectome-comparison/data/rand100_sub_list.txt'
+sublist_filepath = '/gscratch/escience/gkolpin/connectome-comparison/data/rand709_sub_list.txt'
 random_state = 10
 
 # %%
@@ -139,10 +146,12 @@ net17_corr_connectomes = get_connectomes(con_model='correlation_random', network
 # %%
 corr_connectomes = get_connectomes(con_model='correlation_random')
 #corr_connectomes.head()
+print(len(corr_connectomes))
 
 # %%
 lasso_connectomes = get_connectomes(con_model='lassoBIC_blocks')
 #lasso_connectomes.head()
+print(len(lasso_connectomes))
 
 # %%
 #wanting to visualize the connectomes, just get a view, and ideally view the network grouped ones too
@@ -162,6 +171,7 @@ c1 = axs[0].imshow(np.reshape(net7_lasso_connectomes.loc[ids[0]], (7, 7)), vmin=
 c2 = axs[1].imshow(np.reshape(net17_lasso_connectomes.loc[ids[0]], (17, 17)), vmin=-1, vmax=1, cmap='bwr')
 c3 = axs[2].imshow(np.reshape(net7_corr_connectomes.loc[ids[0]], (7, 7)), vmin=-1, vmax=1, cmap='bwr')
 c3 = axs[3].imshow(np.reshape(net17_corr_connectomes.loc[ids[0]], (17, 17)), vmin=-1, vmax=1, cmap='bwr')
+f.tight_layout()
 cbar = f.colorbar(c1, ax=axs, shrink=0.5)
 
 # %%
@@ -275,6 +285,97 @@ net17_corr_data.head()
 
 
 # %%
+#TO DO: add something for a 0 fold thing for linear regression, along with just getting baseline tests
+
+# %%
+#TO DO: try and get CPM working
+def cpm(X, y, folds, correlate, alpha, model_obj, corr_matricies):
+    '''
+    X; connectomes (only makes sense to use the full connectomes)
+    y; predictor
+    folds; folds for total fitting
+    correlate; what method of correlating edges to y (only basic corr now, but want to maybe add partial corr)
+    alpha; p cuttoff for selecting edges to include (I feel like it should be corrected for because we're doing so many tests, but they don't mention that in the shen paper)
+    model; prediciton model (they just do linear regression, but no reason it couldn't be ridge)
+    '''
+    results = {}
+    predictions = []
+    edge_labels = X.columns
+    y = np.array(y)
+    kf = KFold(folds)
+    fold = 0
+    k_fold_y = []
+    for train_index, test_index in kf.split(X):
+        r_vals = []
+        p_vals = []
+        pos_net = set()
+        neg_net = set()
+        sub_sums = pd.DataFrame(index=X.index, columns=['positive', 'negative', 'total'])
+        fold += 1
+    
+        train_X = X.iloc[train_index]
+        train_y = y[train_index]
+        test_X = X.iloc[test_index]
+        test_y = y[test_index]
+    
+        if correlate == 'corr':
+            for edge in edge_labels:
+                r, p = pearsonr(train_X[edge], train_y) #could make this skip the diagonal edges to get rid of the warning, but I dont think it 
+                r_vals.append(r)
+                p_vals.append(p)
+
+        for r, p, edge in zip(r_vals, p_vals, edge_labels):
+            if r > 0 and p < alpha:
+                pos_net.add(edge)
+            elif r < 0 and p < alpha:
+                neg_net.add(edge)
+
+        pos_cols = [edge for edge in edge_labels if edge in pos_net]
+        neg_cols = [edge for edge in edge_labels if edge in neg_net]
+        pos_sum = X[pos_cols].sum(axis=1)
+        neg_sum = X[neg_cols].sum(axis=1)
+        total = pos_sum + neg_sum
+        
+        if corr_matricies:
+            sub_sums['positive'] = pos_sum.values / 2
+            sub_sums['negative'] = neg_sum.values / 2
+            sub_sums['total'] = total.values / 2
+        else:
+            sub_sums['positive'] = pos_sum.values
+            sub_sums['negative'] = neg_sum.values
+            sub_sums['total'] = total.values
+        train_sub_sums = sub_sums.iloc[train_index]
+        test_sub_sums = sub_sums.iloc[test_index]
+        #could theoretically call my model fitting function here, if I make it work with 0 folds (could also additionally crossvalidate this step?)
+        pl = Pipeline([('scaler', StandardScaler()), ('model', model_obj)])
+        pl.fit(train_sub_sums[['total']], train_y)
+        results[f'fold {fold} stats'] = {'test': pl.score(test_sub_sums[['total']], test_y), 'train': pl.score(train_sub_sums[['total']], train_y)}
+        predictions.extend(pl.predict(test_sub_sums[['total']]))
+        k_fold_y.extend(test_y)
+        print(f'fold {fold} complete')
+    
+    results['k_fold_age'] = k_fold_y
+    results['full_r2'] = r2_score(k_fold_y, predictions)
+    results['predictions'] = predictions
+    return(results)
+
+
+# %%
+cpm_corr_results = cpm(corr_data[corr_data.columns[:10000]], corr_data['age'], 5, 'corr', .01, LinearRegression(), True)
+cpm_lasso_results = cpm(lasso_data[corr_data.columns[:10000]], lasso_data['age'], 5, 'corr', .01, LinearRegression(), False)
+print(cpm_corr_results['full_r2'])
+print(cpm_lasso_results['full_r2'])
+
+# %%
+#I don't know if doing CPM on the network connectomes makes sense, usually people do the grouping afterwards from the positive and negative networks.
+
+# %%
+#more testing somewhere else because that took a long time
+train_sub_sums = sub_sums.iloc[train_index]
+print(len(train_sub_sums))
+
+
+# %%
 def fit_model(X, y, model_obj, folds=5):
     '''
     TO DO
@@ -299,6 +400,8 @@ def fit_model(X, y, model_obj, folds=5):
         pl.fit(train_X, train_y).score(test_X, test_y)            
         results[f'fold {fold} stats'] = {'test': pl.score(test_X, test_y), 'train': pl.score(train_X, train_y)}
         results[f'fold {fold} coef'] = model_obj.coef_
+        results[f'fold {fold} alpha'] = model_obj.alpha_
+        
         #can change both of these to maybe be one df that has the id's again as index? maybe just other columnn the way Ariel reccomended
         predictions.extend(pl.predict(test_X))
         k_fold_y.extend(test_y)
@@ -317,30 +420,43 @@ def fit_model(X, y, model_obj, folds=5):
 #    assert index in rand100_phenotype.index
 
 # %%
+n_alphas = 20
+fracs = np.linspace(0, 1, n_alphas)
+model = RidgeCV()
+lasso_results = model.fit(lasso_data[[f'edge_{num}' for num in range(10000)]], lasso_data['age'])
+print(len(lasso_data['age']))
+print(len(model.predict(lasso_data[[f'edge_{num}' for num in range(10000)]])))
+
+# %%
 #for data merging, move things out, be clearer, erorrs can happen there
-model = LassoCV(random_state=random_state)
+folds = 5
+#model = LassoCV(random_state=random_state)
 #model = LinearRegression()
-lasso_results = fit_model(lasso_data[[f'edge_{num}' for num in range(10000)]], lasso_data['age'], model)
-corr_results = fit_model(corr_data[[f'edge_{num}' for num in range(10000)]], corr_data['age'], model)
-net7_lasso_results = fit_model(net7_lasso_data[net7_lasso_connectomes.columns], net7_lasso_data['age'], model)
-net17_lasso_results = fit_model(net17_lasso_data[net17_lasso_connectomes.columns], net17_lasso_data['age'], model)
-net7_corr_results = fit_model(net7_corr_data[net7_corr_connectomes.columns], net7_corr_data['age'], model)
-net17_corr_results = fit_model(net17_corr_data[net17_corr_connectomes.columns], net17_corr_data['age'], model)
+#fracs = np.linspace(0, 1, n_alphas)
+#model = FracRidgeRegressor(fracs=fracs, fit_intercept=True) #fits 20 predictions, one per alpha, sooooo would need to manually select the best alpha?
+model = RidgeCV()
+lasso_results = fit_model(lasso_data[[f'edge_{num}' for num in range(10000)]], lasso_data['age'], model, folds=folds)
+corr_results = fit_model(corr_data[[f'edge_{num}' for num in range(10000)]], corr_data['age'], model, folds=folds)
+net7_lasso_results = fit_model(net7_lasso_data[net7_lasso_connectomes.columns], net7_lasso_data['age'], model, folds=folds)
+net17_lasso_results = fit_model(net17_lasso_data[net17_lasso_connectomes.columns], net17_lasso_data['age'], model, folds=folds)
+net7_corr_results = fit_model(net7_corr_data[net7_corr_connectomes.columns], net7_corr_data['age'], model, folds=folds)
+net17_corr_results = fit_model(net17_corr_data[net17_corr_connectomes.columns], net17_corr_data['age'], model, folds=folds)
 
 # %%
 #Okay soo this can get the coefficient path plots from seemingly where they start changing to all 0
-alphas = np.logspace(-7, 0, 50)
+alphas = np.logspace(-4, 10, 50)
 coefs = []
 for a in alphas:
-    model = Lasso(random_state=random_state, alpha=a)
-    #model.fit(net7_corr_data[net7_corr_data.columns[:49]], net7_corr_data['age']) #looks fairly normal
-    model.fit(net7_lasso_data[net7_corr_data.columns[:49]], net7_lasso_data['age']) #super huge coefficients
+    #model = Lasso(random_state=random_state, alpha=a)
+    model = Ridge(alpha = a)
+    model.fit(net7_corr_data[net7_corr_data.columns[:49]], net7_corr_data['age']) #looks fairly normal
+    #model.fit(net7_lasso_data[net7_corr_data.columns[:49]], net7_lasso_data['age']) #super huge coefficients
     #model.fit(net17_corr_data[net17_corr_data.columns[:289]], net17_corr_data['age'])
     #model.fit(net17_lasso_data[net17_corr_data.columns[:289]], net17_lasso_data['age']) #can lower to -7 for better view, but just bad overal
     #model.fit(corr_data[[f'edge_{num}' for num in range(10000)]], corr_data['age'])
     #model.fit(lasso_data[[f'edge_{num}' for num in range(10000)]], lasso_data['age'])
     coefs.append(model.coef_)
-    
+
 ax = plt.gca()
 
 ax.plot(alphas, coefs)
@@ -348,12 +464,58 @@ ax.set_xscale("log")
 #ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
 plt.xlabel("alpha")
 plt.ylabel("weights")
-plt.title("Ridge Coefficients vs Regularization Strength (alpha)")
+plt.title("Lasso Coefficients vs Regularization Strength (alpha)") #change
 plt.axis("tight")
 plt.show()
 
+
 # %%
-# make some new ones for comparison
+#okay now lasso, with all of these, just seems to be not at all working, so lets try fracridge!
+X = net7_lasso_data[net7_lasso_data.columns[:49]]
+y = net7_lasso_data['age']
+n_alphas = 20
+alphas = np.logspace(-10, 10, n_alphas)
+rr_alphas = np.logspace(-10, 10, n_alphas)
+rr_coefs = []
+rr_coefs = np.zeros((X.shape[-1], n_alphas))
+rr_pred = np.zeros((y.shape[-1], n_alphas))
+
+#just ridge for comparison
+for aa in range(len(rr_alphas)):
+    RR = Ridge(alpha=rr_alphas[aa], fit_intercept=True)
+    RR.fit(X, y)
+    rr_coefs[:, aa] = RR.coef_
+    rr_pred[:, aa] = cross_val_predict(RR, X, y)
+
+#fracridge
+fracs = np.linspace(0, 1, n_alphas)
+FR = FracRidgeRegressor(fracs=fracs, fit_intercept=True)
+FR.fit(X, y)
+fr_pred = cross_val_predict(FR, X, y)
+
+#and plot
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(fracs, FR.coef_.T)
+ylims = ax[0].get_ylim()
+ax[0].vlines(fracs, ylims[0], ylims[1], linewidth=0.5, color='gray')
+ax[0].set_ylim(*ylims)
+ax[1].plot(np.log(rr_alphas[::-1]), rr_coefs.T)
+ylims = ax[1].get_ylim()
+ax[1].vlines(np.log(rr_alphas[::-1]), ylims[0], ylims[1], linewidth=0.5,
+             color='gray')
+ax[1].set_ylim(*ylims)
+
+
+# %%
+'''
+does not work
+X = net7_corr_data[net7_corr_data.columns[:49]]
+y = net7_corr_data['age']
+fracs = np.linspace(0, 1, n_alphas)
+fracridge(X, y, fracs=fracs)
+'''
+
+# %%
 print('lasso:', lasso_results['fold 1 stats'], 'full r2:', lasso_results['full_r2']) #looks just like a lot of overfitting
 print('correlation:', corr_results['fold 1 stats'], 'full r2:', corr_results['full_r2']) #um huh, maybe just underfitting from not enough subjects??? weird b/c still lots of features
 print('lasso 7 networks:', net7_lasso_results['fold 1 stats'], 'full r2:', net7_lasso_results['full_r2'])
@@ -362,7 +524,21 @@ print('correlation 7 networks:', net7_corr_results['fold 1 stats'], 'full r2:', 
 print('correlation 17 networks:', net17_corr_results['fold 1 stats'], 'full r2:', net17_corr_results['full_r2'])
 
 # %%
-data = net7_corr_data
+# make some new ones for comparison
+#fully crossvalidated
+print('lasso: full r2:', lasso_results['full_r2']) #looks just like a lot of overfitting
+print('correlation: full r2:', corr_results['full_r2']) #um huh, maybe just underfitting from not enough subjects??? weird b/c still lots of features
+print('lasso 7 networks: full r2:', net7_lasso_results['full_r2'])
+print('lasso 17 networks: full r2:', net17_lasso_results['full_r2'])
+print('correlation 7 networks: full r2:', net7_corr_results['full_r2'])
+print('correlation 17 networks: full r2:', net17_corr_results['full_r2'])
+
+# %%
+print(net7_corr_results['fold 1 coef'])
+print(net7_corr_results['fold 1 alpha'])
+
+# %%
+data = net7_lasso_data
 #Recreating the RBC figure 7 stuff
 vis_edge = 'SalVentAttn_to_SalVentAttn'
 vis_dfn_edge = 'SalVentAttn_to_Default'
@@ -374,7 +550,7 @@ age_matrix = []
 for edge in net7_corr_data.columns[:49]:
     age_matrix.append(scipy.stats.pearsonr(data[edge], data['age'])[0])
 age_matrix = np.reshape(age_matrix, (7, 7))
-age_matrix = np.tril(age_matrix)
+#age_matrix = np.tril(age_matrix)
 
 sns.heatmap(age_matrix, annot=True, cmap='bwr', fmt='.2f',ax=axs[0], linewidths=0.5, vmin=-0.25, vmax=0.25) 
 axs[0].set_xticks(locations, net7_labels, rotation=45, ha='right', rotation_mode='anchor')
@@ -397,7 +573,10 @@ plt.show()
 #TO DO: check age stratified (maybe like ranging 2-5 buckets?)
 
 # %%
-#TO DO: try and get CPM working (following the shen 2017 paper)
+#TO DO: add ridge and PClasso (and grouplasso (maybe with PCA), and fracridge) to the models and comparisons (really should be up there but down here to remember)
+
+# %%
+#TO DO: get the nilearn correlaiton data visualizations working??? (the cai figure 3) (this is definetly low on priorities rn though)
 
 # %%
 #want to visualize some stuff, but can do that quickly maybe:    
@@ -446,15 +625,20 @@ plt.show()
 
 # %%
 #looking at sparcity of connectomes Ive got
-#need to update this after making the diagonal 0
+cuttoff = 0.65 #max to still get 1 edge is 0.69
 sparce_edges = {}
+diagonal_edges = {}
 for edge_name, edges in lasso_connectomes.items():
     sparcity = (len(edges) - np.count_nonzero(edges)) / len(edges)
     if sparcity >= 0.9:
-        #the term sanity check is kinda great i love it, and need it here but ya seems like im right
+        diagonal_edges[edge_name] = sparcity
+    elif sparcity >= cuttoff:
         sparce_edges[edge_name] = sparcity
-#pprint.pprint(sparce_edges)
 print(len(sparce_edges))
+pprint.pprint(sparce_edges)
+
+# %%
+#TO DO: maybe something thats the opposite of sparsity, like looking at generally strong edges? 
 
 # %%
 
@@ -478,3 +662,5 @@ for val in p:
 p_clean = p[mask]
 print(len(p_clean))
 phenotype_clean = phenotype[mask]
+
+# %%
